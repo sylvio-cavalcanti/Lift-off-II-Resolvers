b63ec8d0a2757a11c785c031e4adc98a5d0d75df
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2024-04-14 16:30:33 -0300

    Fix: resolver file path

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,7 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
-
 const typeDefs = require('./schema');
-const resolvers = require('.resolvers');
+const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api')
 
 async function startApolloServer() {
@@ -11,6 +9,7 @@ async function startApolloServer() {
     typeDefs,
     resolvers,
   });
+
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       const { cache } = server;
